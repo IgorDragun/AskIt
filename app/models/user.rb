b3b4ce0109eb8b0f99a27f323f1a30aa015a507d
Igor Dragun <igor_dragun@bk.ru>
2022-07-01 10:27:15 +0300

    Added remember users using a cookie

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :old_password
+  attr_accessor :old_password, :remember_token
 
   validates :email, presence: true, uniqueness: true, 'valid_email_2/email': true
 
@@ -9,6 +9,27 @@ class User < ApplicationRecord
   validate :correct_old_password, on: :update, if: -> {password.present?}
   validate :password_complexity
   validate :password_presence
+
+  def remember_me
+    self.remember_token = SecureRandom.urlsafe_base64
+    update_column :remember_token_digest, digest(remember_token)
+  end
+
+  def forget_me
+    update_column :remember_token_digest, nil
+    self.remember_token = nil
+  end
+
+  def remember_token_authenticated?(remember_token)
+    return false unless remember_token_digest.present?
+
+    BCrypt::Password.new(remember_token_digest).is_password?(remember_token)
+  end
+
+  def digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 
 
   private
