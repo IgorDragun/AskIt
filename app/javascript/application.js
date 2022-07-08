5fda40f20dc32b98c6e4e9714e22e24c7d1b68b7
Igor Dragun <igor_dragun@bk.ru>
2022-07-08 18:11:38 +0300

    AddeAdded sidekiq autorun with app launch

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,6 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
 
 import 'bootstrap/js/dist/dropdown'
 import 'bootstrap/js/dist/collapse'
